d060ac490b2be06e2171c674eab31b55d561998a
Wilco de Boer <deboer.wilco@gmail.com>
2016-08-11 11:23:56 +0200

    updates

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monocraft_Minecraft_Server_Manager {
+    class Program {
+        static void Main (string[] args) {
+        }
+    }
+}
